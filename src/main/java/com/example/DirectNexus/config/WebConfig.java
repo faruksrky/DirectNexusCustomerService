bcd03b0d5b1fa6ada1bf96a62b6fa1581cbd8eba
faruksarikaya <faruksrky@gmail.com>
2024-10-26 17:38:32 +0300

    https://direct.boosterwizard.com webConfige eklendi

--- src/main/java/com/example/DirectNexus/config/WebConfig.java
+++ src/main/java/com/example/DirectNexus/config/WebConfig.java
@@ -1,43 +1,25 @@
 
 package com.example.DirectNexus.config;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
-@EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 @Configuration
 public class WebConfig {
 
     @Bean
-    public SecurityFilterChain corsConfigurer(HttpSecurity http) throws Exception {
-            http.cors(cors -> cors.configurationSource(request -> {
-                var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-                corsConfiguration.setAllowedOrigins(List.of("http://localhost:3031"));
-                corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-                corsConfiguration.setAllowedHeaders(List.of("*"));
-                corsConfiguration.setAllowCredentials(true);
-                return corsConfiguration;
-            }));
-
-            http.csrf(AbstractHttpConfigurer::disable);
-
-            http.authorizeHttpRequests(authorizeRequests ->
-                            authorizeRequests
-                                    .requestMatchers("/**").permitAll() // Allow unauthenticated access
-                                    .anyRequest().authenticated() // Require authentication for other requests
-                    )
-                    .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-            return http.build();
-        }
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://direct.boosterwizard.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
+
+ */
+
+}

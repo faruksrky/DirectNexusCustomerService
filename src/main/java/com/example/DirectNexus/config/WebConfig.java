3ddc91586fcfaa42589635a9baf0a457665226f2
faruksarikaya <faruksrky@gmail.com>
2024-10-26 17:38:32 +0300

    https://direct.boosterwizard.com webConfige eklendi

--- src/main/java/com/example/DirectNexus/config/WebConfig.java
+++ src/main/java/com/example/DirectNexus/config/WebConfig.java
@@ -5,20 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
-/*
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://www.boostergin.com")
+                        .allowedOrigins("https://direct.boosterwizard.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
-
- */
 
 }

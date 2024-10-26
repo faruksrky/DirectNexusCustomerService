e3734770665902de7bdd57ceab7c31ab0637612c
faruksarikaya <faruksrky@gmail.com>
2024-10-26 16:26:00 +0300

    dev ortamı için cors açıldı

--- src/main/java/com/example/DirectNexus/config/WebConfig.java
+++ src/main/java/com/example/DirectNexus/config/WebConfig.java
@@ -5,20 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig {
-/*
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://www.boostergin.com")
+                        .allowedOrigins("http://localhost:3031")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
-
- */
-
 }

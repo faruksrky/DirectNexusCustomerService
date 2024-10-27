1c3c9d1263c79a0e70042387e02f320b6e0c6f4a
faruksarikaya <faruksrky@gmail.com>
2024-10-27 09:27:28 +0300

    cors ayarları ,için deneme yapılıyor

--- src/main/java/com/example/DirectNexus/config/WebConfig.java
+++ src/main/java/com/example/DirectNexus/config/WebConfig.java
@@ -2,24 +2,28 @@
 package com.example.DirectNexus.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 public class WebConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("https://direct.boosterwizard.com",
-                                "https://customer.boostergin.com",
-                                "https://www.boostergin.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/**").permitAll() // İlk istekte token gereksiz
+                                .anyRequest().authenticated() // Diğer isteklerde kimlik doğrulama gerekli
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-            }
-        };
+        return http.build();
     }
-
 }
+
+

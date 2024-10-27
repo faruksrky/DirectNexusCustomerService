47bc7991d9e46499a30c5a93c9a198023a2f1faa
faruksarikaya <faruksrky@gmail.com>
2024-10-27 09:43:08 +0300

    cors ayarları ,için deneme yapılıyor

--- src/main/java/com/example/DirectNexus/config/WebConfig.java
+++ src/main/java/com/example/DirectNexus/config/WebConfig.java
@@ -2,9 +2,7 @@
 package com.example.DirectNexus.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -18,10 +16,7 @@ public class WebConfig {
                         authorizeRequests
                                 .requestMatchers("/api/**").permitAll() // İlk istekte token gereksiz
                                 .anyRequest().authenticated() // Diğer isteklerde kimlik doğrulama gerekli
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+                );
         return http.build();
     }
 }
